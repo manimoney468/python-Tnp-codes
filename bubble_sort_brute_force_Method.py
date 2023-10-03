28102aae8e82afdda6eee00d64bb29118d527484
manimoney468 <138851793+manimoney468@users.noreply.github.com>
2023-10-03 11:48:33 +0530

    Update bubble_sort_brute_force_Method.py

--- bubble_sort_brute_force_Method.py
+++ bubble_sort_brute_force_Method.py
@@ -1,7 +1,7 @@
 in_arr=[2,3,34,5,67,8,9]
 
 
-for i in range(len(in_arr)):
+for i in range(len(in_arr)-1):#this -1 is for the i=0=n-i-1
     for j in range(0,(len(in_arr)-i-1)):
         if(in_arr[j]>in_arr[j+1]):
             temp=in_arr[j]
