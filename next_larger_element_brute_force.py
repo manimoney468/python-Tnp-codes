009dccc3e37622acc8954c84b78a8e214d34fb43
manimoney468 <138851793+manimoney468@users.noreply.github.com>
2023-10-10 12:21:34 +0530

    Update next_larger_element_brute_force.py

--- next_larger_element_brute_force.py
+++ next_larger_element_brute_force.py
@@ -1,24 +1,21 @@
 l=[12,14,2,3,4,20]
 res=[]
 k="-1"
-for i in range((len(l)+1)):
+for i in range(len(l)):
     ele=l[i]
-    count=0
-    for m in range((i+1),(len(l)+1):
-        bal=len(l)-(i+1)
-        count+=1
+    for m in range((i+1),len(l)):
         if (ele<l[m]):
             res.append(l[m])
             break
-        elif count==bal:
-            res.append(k)
-            break
-    count=0
+    else:
+        res.append(k)
+        
 
 
 
 
-print(res) 
+
+print(res)  
 #javaaaaa like language
 
 """
